9e5d023a8471777995819e70e2e254963d416302
ToureMoustapha <moustapha.toure@etu.univ-amu.fr>
2019-03-06 16:04:33 +0100

    update of Element entity

--- src/decofo/entities/Element.java
+++ src/decofo/entities/Element.java
@@ -5,80 +5,55 @@ import java.util.Map;
 
 import javax.persistence.Basic;
 import javax.persistence.CascadeType;
+import javax.persistence.CollectionTable;
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.MapKeyColumn;
+
 
 @Entity
 public class Element {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
 	private String code;
 
 	@Basic(optional = false)
 	@Column(nullable = false)
-	private String nature;
+	private Nature nature;
 
 	@Basic(optional = false)
 	@Column(nullable = false)
 	private String name;
 
-	private float credits;
-	private float hoursLM;
-	private float hoursTC;
-	private float hoursPW;
+	private Float credits;
+	private Float hoursLM;
+	private Float hoursTC;
+	private Float hoursPW;
+	private Integer thresholdLM;
+	private Integer thresholdTC;
+	private Integer thresholdPW;
 
-	@Basic(optional = false)
-	@Column(nullable = false)
-	private int thresholdLM;
-
-	@Basic(optional = false)
-	@Column(nullable = false)
-	private int thresholdTC;
-
-	@Basic(optional = false)
-	@Column(nullable = false)
-	private int thresholdPW;
-
+	@ElementCollection
+	@CollectionTable(name = "sites_effectifs")
+	@MapKeyColumn(name = "SITE_ID")
 	private Map<Site, Integer> sites;
 
-	@ManyToMany(cascade = { CascadeType.MERGE, CascadeType.PERSIST,
-			CascadeType.REMOVE }, fetch = FetchType.LAZY)
-	@JoinTable(name = "father-child", joinColumns = { @JoinColumn(name = "code") },
-	inverseJoinColumns = { @JoinColumn(name = "code") })
+	@ManyToMany(cascade = { CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REMOVE }, fetch = FetchType.LAZY)
+	@JoinTable(name = "father-child", joinColumns = { @JoinColumn(name = "code") }, inverseJoinColumns = {
+			@JoinColumn(name = "code") })
 	private List<Element> children;
 
 	@ManyToMany(mappedBy = "children", fetch = FetchType.LAZY)
 	private List<Element> fathers;
-	
 
 	public Element() {
 		super();
-	}
-
-	public Element(String nature, String name, float hoursLM, float hoursTC, float hoursPW, int thresholdLM,
-			int thresholdTC, int thresholdPW, float credits, Map<Site, Integer> sites, List<Element> fathers,
-			List<Element> children) {
-		super();
-		this.nature = nature;
-		this.name = name;
-		this.hoursLM = hoursLM;
-		this.hoursTC = hoursTC;
-		this.hoursPW = hoursPW;
-		this.thresholdLM = thresholdLM;
-		this.thresholdTC = thresholdTC;
-		this.thresholdPW = thresholdPW;
-		this.credits = credits;
-		this.sites = sites;
-		this.fathers = fathers;
-		this.children = children;
 	}
 
 	public String getCode() {
@@ -89,11 +64,11 @@ public class Element {
 		this.code = code;
 	}
 
-	public String getNature() {
+	public Nature getNature() {
 		return nature;
 	}
 
-	public void setNature(String nature) {
+	public void setNature(Nature nature) {
 		this.nature = nature;
 	}
 
@@ -105,60 +80,60 @@ public class Element {
 		this.name = name;
 	}
 
-	public float getHoursLM() {
-		return hoursLM;
-	}
-
-	public void setHoursLM(float hoursLM) {
-		this.hoursLM = hoursLM;
-	}
-
-	public float getHoursTC() {
-		return hoursTC;
-	}
-
-	public void setHoursTC(float hoursTC) {
-		this.hoursTC = hoursTC;
-	}
-
-	public float getHoursPW() {
-		return hoursPW;
-	}
-
-	public void setHoursPW(float hoursPW) {
-		this.hoursPW = hoursPW;
-	}
-
-	public int getThresholdLM() {
-		return thresholdLM;
-	}
-
-	public void setThresholdLM(int thresholdLM) {
-		this.thresholdLM = thresholdLM;
-	}
-
-	public int getThresholdTC() {
-		return thresholdTC;
-	}
-
-	public void setThresholdTC(int thresholdTC) {
-		this.thresholdTC = thresholdTC;
-	}
-
-	public int getThresholdPW() {
-		return thresholdPW;
-	}
-
-	public void setThresholdPW(int thresholdPW) {
-		this.thresholdPW = thresholdPW;
-	}
-
-	public float getCredits() {
+	public Float getCredits() {
 		return credits;
 	}
 
-	public void setCredits(float credits) {
+	public void setCredits(Float credits) {
 		this.credits = credits;
+	}
+
+	public Float getHoursLM() {
+		return hoursLM;
+	}
+
+	public void setHoursLM(Float hoursLM) {
+		this.hoursLM = hoursLM;
+	}
+
+	public Float getHoursTC() {
+		return hoursTC;
+	}
+
+	public void setHoursTC(Float hoursTC) {
+		this.hoursTC = hoursTC;
+	}
+
+	public Float getHoursPW() {
+		return hoursPW;
+	}
+
+	public void setHoursPW(Float hoursPW) {
+		this.hoursPW = hoursPW;
+	}
+
+	public Integer getThresholdLM() {
+		return thresholdLM;
+	}
+
+	public void setThresholdLM(Integer thresholdLM) {
+		this.thresholdLM = thresholdLM;
+	}
+
+	public Integer getThresholdTC() {
+		return thresholdTC;
+	}
+
+	public void setThresholdTC(Integer thresholdTC) {
+		this.thresholdTC = thresholdTC;
+	}
+
+	public Integer getThresholdPW() {
+		return thresholdPW;
+	}
+
+	public void setThresholdPW(Integer thresholdPW) {
+		this.thresholdPW = thresholdPW;
 	}
 
 	public Map<Site, Integer> getSites() {
@@ -169,20 +144,20 @@ public class Element {
 		this.sites = sites;
 	}
 
-	public List<Element> getFathers() {
-		return fathers;
-	}
-
-	public void setFathers(List<Element> fathers) {
-		this.fathers = fathers;
-	}
-
 	public List<Element> getChildren() {
 		return children;
 	}
 
 	public void setChildren(List<Element> children) {
 		this.children = children;
+	}
+
+	public List<Element> getFathers() {
+		return fathers;
+	}
+
+	public void setFathers(List<Element> fathers) {
+		this.fathers = fathers;
 	}
 
 }
