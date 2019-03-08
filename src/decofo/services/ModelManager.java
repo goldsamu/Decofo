d2c0bd34ab5d72ad3fc46b1f8d26189bc03b77bb
mahdi hassan djilal <mahdi hassan djilal@DESKTOP-EQ0BI02>
2019-03-08 14:33:53 +0100

    Creating ModelManager

--- src/decofo/services/ModelManager.java
+++ src/decofo/services/ModelManager.java
@@ -1,8 +1,48 @@
 package decofo.services;
 
-import javax.ejb.Stateless;
+import java.util.List;
 
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import decofo.entities.Model;
+
+/**
+ * 
+ * @author mahdi hassan djilal
+ *
+ */
 @Stateless
 public class ModelManager {
-	
+
+	@PersistenceContext(unitName = "decofoDatabaseUnit")
+	private EntityManager em;
+
+	public List<Model> findCourses() {
+		return em.createQuery("Select m From Model m", Model.class).getResultList();
+	}
+
+	public Model findCourse(String code) {
+		return em.find(Model.class, code);
+	}
+
+	public Model saveCourse(Model m) {
+		if (m.getCode() == null) {
+			em.persist(m);
+		} else {
+			m = em.merge(m);
+		}
+		return m;
+	}
+
+	public void deleteCourse(Model m) {
+		m = em.merge(m);
+		em.remove(m);
+	}
+
+	public Model updatePerson(Model m) {
+		m = em.merge(m);
+		return m;
+	}
 }
