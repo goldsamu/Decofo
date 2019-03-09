fb2769f92eafbbae1935980d1bf02787385a4f3f
goldsamu <samuel.goldstein11@gmail.com>
2019-03-09 17:12:40 +0100

    Update Person.java

--- src/decofo/entities/Person.java
+++ src/decofo/entities/Person.java
@@ -1,23 +1,11 @@
 package decofo.entities;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.persistence.Basic;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.PostUpdate;
-import javax.persistence.PreUpdate;
-import javax.persistence.Transient;
 import javax.persistence.Version;
 
 @Entity
