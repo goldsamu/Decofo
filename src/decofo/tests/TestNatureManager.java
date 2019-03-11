229df3c1683bdba23df9b19c4a6f2a2007d2fe63
ToureMoustapha <moustapha.toure@etu.univ-amu.fr>
2019-03-11 17:45:27 +0100

    Adding SiteManager and TestSiteManager

---
+++ src/decofo/tests/TestNatureManager.java
@@ -0,0 +1,52 @@
+package decofo.tests;
+
+import static org.junit.Assert.assertNotNull;
+
+import javax.ejb.embeddable.EJBContainer;
+import javax.naming.NamingException;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import decofo.entities.Element;
+import decofo.entities.Nature;
+import decofo.services.NatureManager;
+
+public class TestNatureManager {
+
+	static EJBContainer container;
+	static NatureManager nm;
+
+	@BeforeClass
+	public static void beforeAll() throws NamingException {
+		final String name = "java:global/Decofo/NatureManager";
+		container = EJBContainer.createEJBContainer();
+		nm = (NatureManager) container.getContext().lookup(name);
+	}
+
+	@AfterClass
+	public static void afterAll() {
+		container.close();
+	}
+
+	@Test
+	public void testInject() {
+		Assert.assertNotNull(nm);
+	}
+	
+	@Test
+	public void testCreateAndFindNature() {
+		
+		Nature nature1 = new Nature("codeNature1", "nameNature1");
+		Nature nature2 = new Nature("codeNature2", "nameNature2");
+		
+		nm.createNature(nature1);
+		nm.createNature(nature2);
+	
+		Nature natureFromDatabase = nm.findNature("codeNature2");
+		assertNotNull(natureFromDatabase);
+		Assert.assertEquals(natureFromDatabase.getName(), nature2.getName());
+	}
+}
---
+++ src/decofo/tests/TestSiteManager.java
@@ -0,0 +1,52 @@
+package decofo.tests;
+
+import static org.junit.Assert.assertNotNull;
+
+import javax.ejb.embeddable.EJBContainer;
+import javax.naming.NamingException;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import decofo.entities.Nature;
+import decofo.entities.Site;
+import decofo.services.SiteManager;
+
+public class TestSiteManager {
+
+	static EJBContainer container;
+	static SiteManager sm;
+
+	@BeforeClass
+	public static void beforeAll() throws NamingException {
+		final String name = "java:global/Decofo/SiteManager";
+		container = EJBContainer.createEJBContainer();
+		sm = (SiteManager) container.getContext().lookup(name);
+	}
+
+	@AfterClass
+	public static void afterAll() {
+		container.close();
+	}
+
+	@Test
+	public void testInject() {
+		Assert.assertNotNull(sm);
+	}
+	
+	@Test
+	public void testCreateAndFindSite() {
+		
+		Site site1 = new Site("codeSite1", "nameSite1");
+		Site site2 = new Site("codeSite2", "nameSite2");
+		
+		sm.createSite(site1);
+		sm.createSite(site2);
+	
+		Site siteFromDatabase = sm.findSite("codeSite2");
+		assertNotNull(siteFromDatabase);
+		Assert.assertEquals(siteFromDatabase.getName(), site2.getName());
+	}
+}
