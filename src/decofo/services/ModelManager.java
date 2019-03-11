87d67c92d86d60ea1d929c73a6435deeacd769a9
mahdi hassan djilal <mahdi hassan djilal@DESKTOP-EQ0BI02>
2019-03-11 23:55:37 +0100

    add the method to test admin

--- src/decofo/services/ModelManager.java
+++ src/decofo/services/ModelManager.java
@@ -16,28 +16,36 @@ import decofo.entities.Person;
  */
 @Stateless
 public class ModelManager {
-	
+
 	@PersistenceContext(unitName = "decofoDatabaseUnit")
 	private EntityManager em;
 	private PersonManager pm;
+
 	/**
 	 * to return a list of model
+	 * 
 	 * @return List<Model> liste of Model that is saved in the base
 	 */
 	public List<Model> findModel() {
 		return em.createQuery("Select m From Model m", Model.class).getResultList();
 	}
+
 	/**
 	 * to find a model that it code is passed on argument
-	 * @param code code of the model that we want to find it
+	 * 
+	 * @param code
+	 *            code of the model that we want to find it
 	 * @return Model model that it code is specified on argument
 	 */
 	public Model findModel(String code) {
 		return em.find(Model.class, code);
 	}
+
 	/**
 	 * to save a model
-	 * @param m the model that we want to save
+	 * 
+	 * @param m
+	 *            the model that we want to save
 	 * @return Model saved model
 	 */
 	public Model saveModel(Model m) {
@@ -48,46 +56,69 @@ public class ModelManager {
 		}
 		return m;
 	}
+
 	/**
 	 * to destroy the model that is passed on argument
-	 * @param m model to destroy
+	 * 
+	 * @param m
+	 *            model to destroy
 	 */
 	public void deleteModel(Model m) {
 		m = em.merge(m);
 		em.remove(m);
 	}
+
 	/**
 	 * to udpate a model
-	 * @param m the model that we want to update
+	 * 
+	 * @param m
+	 *            the model that we want to update
 	 * @return Model updated model
 	 */
 	public Model updateModel(Model m) {
 		m = em.merge(m);
 		return m;
 	}
+
 	/**
-	 * this methode test when an user want to update a model if this user is the responsible of the model on update. 
-	 * @param m is an object of Model
+	 * this methode test when an user want to update a model if this user is the
+	 * responsible of the model on update.
+	 * 
+	 * @param m
+	 *            is an object of Model
 	 * @return a boolean(True or False)
 	 */
 	public boolean isResponsible(Model m) {
 		boolean response = false;
-		if(m.getResponsables().size()==1) {
-			if(m.getResponsables().get(0).getLogin() == pm.getUser().getLogin()) {
+		if (m.getResponsables().size() == 1) {
+			if (m.getResponsables().get(0).getLogin() == pm.getUser().getLogin()) {
 				response = true;
-			}else {
+			} else {
 				response = false;
 			}
-		}else {
-			for(Person p:m.getResponsables()) {
-				if(p.getLogin() == pm.getUser().getLogin()) {
+		} else {
+			for (Person p : m.getResponsables()) {
+				if (p.getLogin() == pm.getUser().getLogin()) {
 					response = true;
-				}else {
+				} else {
 					response = false;
 				}
 			}
 		}
-		
+
 		return response;
+	}
+	/**
+	 * this method test if an user is an administrator or not
+	 * @return admin
+	 */
+	public boolean isAdmin() {
+		boolean admin;
+		if (pm.getUser().isAdmin()) {
+			admin = true;
+		} else {
+			admin = false;
+		}
+		return admin;
 	}
 }
