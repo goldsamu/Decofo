61e9684606a96c7b2263f48cf0bbcb145cd0df3a
ToureMoustapha <moustapha.toure@etu.univ-amu.fr>
2019-03-12 17:22:02 +0100

    Modification of Model class

--- src/decofo/entities/Model.java
+++ src/decofo/entities/Model.java
@@ -18,23 +18,22 @@ public class Model implements Serializable {
 	 * the Model entity attributs
 	 */
 	@Id
-	@Basic(optional = false)
 	private String code;
 
 	@Basic(optional = false)
-	private String nom;
+	private String name;
 	
-	@OneToMany(fetch = FetchType.LAZY)
-	private List<Person> responsables;
+	@ManyToMany(cascade = { CascadeType.PERSIST, CascadeType.MERGE}, fetch = FetchType.LAZY)
+	private List<Person> responsibles;
 	
-	@OneToMany(cascade = { CascadeType.ALL}, fetch = FetchType.LAZY)
+	@OneToMany(cascade = {CascadeType.ALL}, fetch = FetchType.LAZY)
 	private List<Element> elements;
 
 	//the Model entity contructors
 
 	public Model() {
 		super();
-		this.responsables = new ArrayList<Person>();
+		this.responsibles = new ArrayList<Person>();
 		this.elements = new ArrayList<Element>();
 	}
 	/**
@@ -42,11 +41,11 @@ public class Model implements Serializable {
 	 * @param code Model code
 	 * @param nom  Model Name
 	 */
-	public Model(String code, String nom) {
+	public Model(String code, String name) {
 		super();
 		this.code = code;
-		this.nom = nom;
-		this.responsables = new ArrayList<Person>();
+		this.name = name;
+		this.responsibles = new ArrayList<Person>();
 		this.elements = new ArrayList<Element>();
 	}
 	
@@ -69,29 +68,29 @@ public class Model implements Serializable {
 	 * return name of Model
 	 * @return Nom
 	 */
-	public String getNom() {
-		return nom;
+	public String getName() {
+		return name;
 	}
 	/**
 	 * to set name of Model
 	 * @param nom
 	 */
-	public void setNom(String nom) {
-		this.nom = nom;
+	public void setName(String nom) {
+		this.name = nom;
 	}
 	/**
 	 * to have the list of Model responsible
 	 * @return responsibles
 	 */
-	public List<Person> getResponsables() {
-		return responsables;
+	public List<Person> getResponsibles() {
+		return responsibles;
 	}
 	/**
 	 * to set the list of person who edited the Model
 	 * @param responsables
 	 */
-	public void setResponsables(List<Person> responsables) {
-		this.responsables = responsables;
+	public void setResponsibles(List<Person> responsables) {
+		this.responsibles = responsables;
 	}
 	/**
 	 * to return the list of elements that the model contains
@@ -107,11 +106,10 @@ public class Model implements Serializable {
 	public void setElements(List<Element> elements) {
 		this.elements = elements;
 	}
-	
 	//the toString method to have a full descrition of Model entity
 	@Override
 	public String toString() {
-		return "Model [code=" + code + ", nom=" + nom + ", responsables=" + responsables + ", elements=" + elements
+		return "Model [Code=" + code + ", Name=" + name + ", responsibles=" + responsibles + ", Elements=" + elements
 				+ "]";
 	}
 }
