e5fbac92473137d44e3af7d1c982ab6a57c9640d
ToureMoustapha <moustapha.toure@etu.univ-amu.fr>
2019-03-13 14:41:29 +0100

    add findNatureByName function

--- src/decofo/services/NatureManager.java
+++ src/decofo/services/NatureManager.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 import decofo.entities.Nature;
 
@@ -13,16 +14,21 @@ public class NatureManager {
 
 	@PersistenceContext(unitName = "myTestDatabaseUnit")
 	private EntityManager em;
-	
+
 	public Nature createNature(Nature m) {
 		em.persist(em.contains(m) ? m : em.merge(m));
 		return m;
 	}
-	
+
 	public Nature findNature(String code) {
 		return em.find(Nature.class, code);
 	}
-	
+
+	public Nature findNatureByName(String name) {
+		TypedQuery<Nature> q = em.createQuery("From Nature n where n.name = '" + name + "'", Nature.class);
+		return q.getSingleResult();
+	}
+
 	public List<Nature> findAllNature() {
 		return em.createQuery("From Nature", Nature.class).getResultList();
 	}
